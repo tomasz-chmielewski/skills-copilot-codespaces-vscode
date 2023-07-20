f1e0bebe1e9441dd4fd1b9d9793380f8117c1049
tomasz-chmielewski <47078749+tomasz-chmielewski@users.noreply.github.com>
2023-07-20 06:09:59 +0000

    Copilot second commit

--- member.js
+++ member.js
@@ -1,9 +1,6 @@
-function skiilsMember() {
-    return {
-        restrict: 'E',
-        templateUrl: 'app/components/member/member.html',
-        controller: 'MemberController',
-        controllerAs: 'vm',
-        bindToController: true
-    };
+function skillsMember() {
+  return {
+    restrict: 'E',
+    templateUrl: 'templates/skills-member.html'
+  };
 }
